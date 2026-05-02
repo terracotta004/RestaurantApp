bd1b7be98610d420db05cece412f694871d72ea9
Ben Underwood <ben@underwood.solutions>
2026-05-02 10:26:58 -0400

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<Db>((sp, options) =>
+{
+    options.UseNpgsql("Host=localhost;Port=5432;Username=restaurant_user;Password=restaurant;Database=restaurant");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+// app.Run();
+
+builder.Services.AddDbContext<Db>((sp, options) =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
+
+await using var scope = app.Services.CreateAsyncScope();
+var db = scope.ServiceProvider.GetRequiredService<Db>();
+var canConnect = await db.Database.CanConnectAsync();
+app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+public class Db(DbContextOptions<Db> options) : DbContext(options)
+{
+
+}
+
